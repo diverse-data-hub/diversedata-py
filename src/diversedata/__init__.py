a89447ec3e02df2f9ce2137c5fa353070c2e8a07
Stephanie-Ta <stephaniecarlynta@gmail.com>
2025-07-09 15:44:32 -0700

    populate package namespace

--- src/diversedata/__init__.py
+++ src/diversedata/__init__.py
@@ -1,3 +1,12 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("diversedata")
+
+# populate package namespace
+from diversedata.diversedata import load_data, list_data
+from diversedata._bcindigenousbiz import load_bcindigenousbiz
+from diversedata._genderassessment import load_genderassessment
+from diversedata._globalrights import load_globalrights
+from diversedata._hcmst import load_hcmst
+from diversedata._wildfire import load_wildfire
+from diversedata._womensmarchmadness import load_womensmarchmadness
