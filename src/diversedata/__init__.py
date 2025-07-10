b67797fa70b519f5127a54de40ced838b33daaea
Stephanie-Ta <stephaniecarlynta@gmail.com>
2025-07-10 09:42:04 -0700

    move data descriptions to txt files and remove functions per dataset

--- src/diversedata/__init__.py
+++ src/diversedata/__init__.py
@@ -3,10 +3,4 @@ from importlib.metadata import version
 __version__ = version("diversedata")
 
 # populate package namespace
-from diversedata.diversedata import load_data, list_available_datasets
-from diversedata._bcindigenousbiz import load_bcindigenousbiz
-from diversedata._genderassessment import load_genderassessment
-from diversedata._globalrights import load_globalrights
-from diversedata._hcmst import load_hcmst
-from diversedata._wildfire import load_wildfire
-from diversedata._womensmarchmadness import load_womensmarchmadness
+from diversedata.diversedata import load_data, list_available_datasets, print_data_description
