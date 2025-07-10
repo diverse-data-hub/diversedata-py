a9c05bcabae29dd8bd2c6a681cd2e380adb2da90
Stephanie-Ta <stephaniecarlynta@gmail.com>
2025-07-10 10:21:56 -0700

    add function to read the data description

--- src/diversedata/diversedata.py
+++ src/diversedata/diversedata.py
@@ -5,8 +5,8 @@ def load_data(data_name: str):
     """
     Returns a pandas.DataFrame of a .csv file that is bundled in this package.
 
-    Looks for data_name.csv in this package's resources and 
-    raises an error if the file is missing.
+    Looks for data_name.csv in this package's resources (in the data directory)
+    and raises an error if data_name is not a string or if the file is missing.
 
     Parameters
     ----------
@@ -21,8 +21,10 @@ def load_data(data_name: str):
 
     Raises
     ------
+    TypeError
+        If data_name is not a string.
     FileNotFoundError
-        If data_name cannot be found in this package.
+        If data_name.csv cannot be found in this package.
 
     Example
     --------
@@ -32,6 +34,10 @@ def load_data(data_name: str):
        year     fire_number     current_size  ...
     0  2006     PWF001          0.1           ...
     """
+    # Raise TypeError if data_name is not a string
+    if not isinstance(data_name, str):
+        raise TypeError(f"{data_name} must be a string with no file extension.")
+    
     # Get the root package name
     package_name = __name__.split('.')[0]
     
@@ -40,11 +46,57 @@ def load_data(data_name: str):
     
     try:
         # Use importlib.resources to access the file in this package
-        with resources.open_text(package_name, file_path) as file:
-            data = pd.read_csv(file)
+        with resources.files(package_name).joinpath(file_path).open("r", encoding="utf-8") as data_file:
+            data = pd.read_csv(data_file)
         return data
     except FileNotFoundError:
         raise FileNotFoundError(f"{data_name}.csv is not found in the {package_name} package.")
+
+def print_data_description(data_name: str):
+    """
+    Prints the description of a dataset contained in a .txt file that is bundled in this package
+
+    Looks for data_name.txt in this package's resources (in the data_descriptions directory)
+    and raises an error if data_name is not a string or if the file is missing.
+
+    Parameters
+    ----------
+    data_name : str
+        The filename (excluding the .txt extension) of the data description file to print.
+
+    Raises
+    ------
+    TypeError
+        If data_name is not a string.
+    FileNotFoundError
+        If data_name.txt cannot be found in this package.
+    
+    Example
+    --------
+    >>> import diversedata as dd
+    >>> dd.print_data_description("wildfire")
+    Alberta Historical Wildfire Dataset (2006-2024).
+
+    This dataset contains detailed records of wildfire...
+    """
+    # Raise TypeError if data_name is not a string
+    if not isinstance(data_name, str):
+        raise TypeError(f"{data_name} must be a string with no file extension.")
+    
+    # Get the root package name
+    package_name = __name__.split('.')[0]
+
+    # Construct the file path
+    file_path = f"data_descriptions/{data_name}.txt"
+    
+    try:
+        # Use importlib.resources to access the file in this package
+        with resources.files(package_name).joinpath(file_path).open('r', encoding='utf-8') as description_file:
+            description = description_file.read()
+            print(description, end='')
+    except FileNotFoundError:
+        print("{data_name}.txt is not found in the {package_name} package.")
+
 
 def list_available_datasets():
     """
